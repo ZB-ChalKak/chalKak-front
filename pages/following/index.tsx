e8869d363fc6f30879ee86e81e6f92b6840abe91
khakisage <jongjin201@gmail.com>
2023-09-09 16:12:15 +0900

    🔧 [Chore] : 불필요 코드 삭제

--- pages/following/index.tsx
+++ pages/following/index.tsx
@@ -73,17 +73,15 @@ const Following = () => {
     };
     fetchFollowingPosts();
   }, [accessToken]);
-  const postsLength = followingPosts.length;
-  const firstCols = followingPosts.slice(0, postsLength / 2);
-  const secondCols = followingPosts.slice(postsLength / 2, postsLength);
-  console.log("firstCols", firstCols);
-  console.log("secondCols", secondCols);
+
   return (
     <div className="w-full h-full bg-white">
       <div className="mx-auto">
         <div className="flex items-center justify-start border-b pb-2">
-          <button className="mr-4 text-lg ml-6">추천</button>
-          <button className="text-lg text-white bg-black rounded">팔로잉</button>
+          <button className="mr-4 ml-6 text-lg font-semibold">팔로잉</button>
+          <button className="text-lg text-gray-400" onClick={() => router.push("/main")}>
+            추천
+          </button>
         </div>
         <div style={{ columnCount: 2, columnGap: "1rem", padding: "0 1rem" }} className="mt-4">
           {followingPosts.map((post) => (
@@ -99,7 +97,7 @@ const Following = () => {
                 <div className="flex flex-row items-center justify-between w-full gap-1 mb-2 mt-2 ml-4">
                   <div className="flex flex-row items-center justify-start">
                     <div className="avatar">
-                      <div className="w-8 h-8 rounded-full">
+                      <div className="w-8 h-8 rounded-full" onClick={() => router.push(`/userinfo/${post.writer.id}`)}>
                         <img src={post.writer.profileImg} alt="profile-img" />
                       </div>
                     </div>
@@ -146,33 +144,3 @@ const Following = () => {
 };
 
 export default Following;
-
-// .text-2xl {
-//   font-size: 1.5rem/* 24px */;
-//   line-height: 2rem/* 32px */;
-// }
-
-{
-  /* <div className="mt-5 h-auto grid grid-cols-2 gap-4 px-4 grid-auto-rows: 1fr">
-  {followingPosts.map((post) => (
-    <div className="grid gap-4" key={post.id}>
-      <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-row items-center justify-start w-full gap-4 mb-2">
-          <div className="avatar">
-            <div className="w-10 h-10 rounded-full">
-              <img src={post.writer.profileImg} alt="profile-img" />
-            </div>
-          </div>
-          <div>
-            <p className="text-xs">{post.writer.nickname}</p>
-          </div>
-        </div>
-        <img src={post.thumbnail} alt="post-img" style={{ objectFit: "cover", height: "100%" }} />
-        <div>{post.content}</div>
-        <div>#{post.hashTags}</div>
-        <div>#{post.styleTags}</div>
-      </div>
-    </div>
-  ))}
-</div>; */
-}
