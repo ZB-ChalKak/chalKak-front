187d9163dfe0c4769249d22d1701429171feae00
khakisage <jongjin201@gmail.com>
2023-09-12 08:59:03 +0900

    ✨ [Feat] : 사용자 작성 게시글 무한 스크롤 구현 중_1

--- pages/userinfo/[userId].tsx
+++ pages/userinfo/[userId].tsx
@@ -1,6 +1,5 @@
-// import { Post } from "@/utils/type";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { apiInstance } from "../api/api";
 import Cookies from "js-cookie";
 import { useRecoilState } from "recoil";
@@ -9,8 +8,6 @@ import { LiaUserCircleSolid } from "react-icons/lia";
 import dynamic from "next/dynamic";
 import FollowerModal from "./FollowerModal";
 import FollowingModal from "./FollowingModal";
-import { useInfiniteQuery } from "react-query";
-import { userPostsType } from "@/utils/type";
 
 export default function UserInfo(): JSX.Element {
   const [userDetail, setUserDetail] = useRecoilState(userDetailState);
@@ -19,11 +16,13 @@ export default function UserInfo(): JSX.Element {
   const [following, setFollowing] = useRecoilState(followingListState);
   const [isOpenFollowerModal, setIsOpenFollowerModal] = useState<boolean>(false);
   const [isOpenFollowingModal, setIsOpenFollowingModal] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasPosts, setHasPosts] = useState<boolean>(false);
   const curUserId = Cookies.get("userId");
   const router = useRouter();
   const { userId } = router.query;
-  console.log("following", following);
-  console.log("followers", followers);
+  const observerRef = useRef<IntersectionObserver | null>(null);
+
   // 사용자 정보 조회 api (프로필 이미지, 게시글 수, 팔로워 수, 팔로잉 수)
   useEffect(() => {
     const fetchUserDetail = async () => {
@@ -39,49 +38,93 @@ export default function UserInfo(): JSX.Element {
     }
   }, [userId]);
 
-  // 사용자 작성 게시글 조회 api
+  // 사용자 작성 게시글 조회 api (initial)
+  useEffect(() => {
+    // const { userId } = router.query;
+    const fetchUserPosts = async () => {
+      try {
+        const userPostsRes = await apiInstance.get(`/users/${userId}/posts?page=0&size=3`);
+        setUserPosts(userPostsRes.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchUserPosts();
+  }, [userId]);
+
   // useEffect(() => {
-  //   // const { userId } = router.query;
-  //   const fetchUserPosts = async () => {
-  //     try {
-  //       const userPostsRes = await apiInstance.get(`/users/${userId}/posts?page=0&size=12`);
-  //       setUserPosts(userPostsRes.data.data);
-  //     } catch (err) {
-  //       console.log(err);
+  //   const handleIntersection = async (entries: IntersectionObserverEntry[]) => {
+  //     const entry = entries[0];
+  //     if (entry.isIntersecting && hasPosts && !isLoading) {
+  //       try {
+  //         setIsLoading(true);
+  //         const nextPage = userPosts.currentPage + 1;
+  //         const userPostsRes = await apiInstance.get(`/users/${userId}/posts?page=${nextPage}&size=6`);
+  //         const newPosts = userPostsRes.data.data.posts;
+  //         if (newPosts.length > 0) {
+  //           setUserPosts((prev) => ({
+  //             ...prev,
+  //             posts: [...prev.posts, ...userPostsRes.data.data.posts],
+  //           }));
+  //         } else {
+  //           setHasPosts(false);
+  //         }
+  //       } catch (err) {
+  //         console.log(err);
+  //       } finally {
+  //         setIsLoading(false);
+  //       }
   //     }
   //   };
 
-  //   if (userId) {
-  //     fetchUserPosts();
-  //   }
-  // }, [userId]);
-  const fetchUserPosts = async ({ pageParam = 0 }) => {
-    try {
-      const userPostsRes = await apiInstance.get(`/users/${userId}/posts?page=${pageParam}&size=12`);
-      return userPostsRes.data.data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  //   observerRef.current = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
 
-  const {
-    data: userPostsData,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-  } = useInfiniteQuery("userPosts", fetchUserPosts, {
-    getNextPageParam: (lastPage) => lastPage.page + 1,
-  });
+  //   if (observerRef.current && userDetail.postsCount > 0) {
+  //     observerRef.current.observe(document.getElementById("scroll-trigger") as Element);
+  //   }
+
+  //   return () => {
+  //     if (observerRef.current) observerRef.current.disconnect();
+  //   };
+  // }, [userDetail.postsCount, hasPosts, isLoading, userPosts.currentPage]);
 
   useEffect(() => {
-    if (userPostsData) {
-      const allPages: userPostsType[] = userPostsData.pages.flatMap((page) => page.posts);
-      setUserPosts((prev) => ({
-        ...prev,
-        posts: allPages,
-      }));
+    const handleIntersection = async (entries: IntersectionObserverEntry[]) => {
+      const entry = entries[0];
+      if (entry.isIntersecting && hasPosts && !isLoading) {
+        try {
+          setIsLoading(true);
+          const nextPage = userPosts.currentPage + 1;
+          const userPostsRes = await apiInstance.get(`/users/${userId}/posts?page=${nextPage}&size=6`);
+          const newPosts = userPostsRes.data.data.posts;
+          if (newPosts.length > 0) {
+            setUserPosts((prev) => ({
+              ...prev,
+              currentPage: nextPage,
+              posts: [...prev.posts, ...userPostsRes.data.data.posts],
+            }));
+          } else {
+            setHasPosts(false);
+          }
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    observerRef.current = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
+    if (observerRef.current && userDetail.postsCount > 0) {
+      observerRef.current.observe(document.getElementById("scroll-trigger") as Element);
     }
-  }, [userPostsData]);
+
+    return () => {
+      if (observerRef.current) observerRef.current.disconnect();
+    };
+  }, [userDetail.postsCount, hasPosts, isLoading, userPosts.currentPage]);
 
   // followerList 조회 api
   useEffect(() => {
@@ -131,14 +174,14 @@ export default function UserInfo(): JSX.Element {
     setIsOpenFollowingModal(false);
   };
 
-  console.log("userpostss", userPosts.posts);
+  // console.log("userpostss", userPosts.posts);
   console.log(userDetail);
   const moveToModifyPage = () => {
     router.push(`/modify-userinfo/${userId}`);
   };
   const DynamicBtn = dynamic(() => import("../modify-userinfo/ModifyButton"), { ssr: false });
   // const gridRowCount = Math.ceil(tempPosts.length / 3);
-  // const { posts } = userPosts;
+  const { posts } = userPosts;
   // const gridRowCount = Math.ceil(posts.length / 3);
   return (
     <div className="flex flex-col w-full items-center justify-center">
@@ -149,11 +192,6 @@ export default function UserInfo(): JSX.Element {
               <div className="w-32 rounded-full">
                 {userDetail.profileImg === "" && <LiaUserCircleSolid className="w-40 h-40" />}
                 {userDetail.profileImg !== "" && <img src={userDetail.profileImg} alt="profile-img" />}
-                {/* {userDetail.profileImg !== "" ? (
-                  <LiaUserCircleSolid className="w-40 h-40" />
-                ) : (
-                  <img src={userDetail.profileImg} alt="profile-img" />
-                )} */}
               </div>
             </div>
             <div className="flex flex-col gap-10 items-center p-5">
@@ -164,12 +202,10 @@ export default function UserInfo(): JSX.Element {
               <div className="flex flex-row justify-between items-center gap-10 ">
                 <div className="flex flex-col items-center text-sm text-black">
                   <p>게시글</p>
-                  {/* {postCount} */}
                   {userDetail.postsCount}
                 </div>
                 <div className="flex flex-col items-center text-sm text-black" onClick={openFollowerModal}>
                   <p>팔로워</p>
-                  {/* {followers.length} */}
                   {userDetail.followerCount}
                 </div>
                 {isOpenFollowerModal && (
@@ -181,7 +217,6 @@ export default function UserInfo(): JSX.Element {
                 )}
                 <div className="flex flex-col items-center text-sm text-black" onClick={openFollowingModal}>
                   <p>팔로잉</p>
-                  {/* {following.length} */}
                   {userDetail.followingCount}
                 </div>
                 {isOpenFollowingModal && (
@@ -195,25 +230,10 @@ export default function UserInfo(): JSX.Element {
             </div>
           </div>
         </div>
-        <div className="mt-[10px] pt-2 border-t-2">
+        <div className="mt-[10px] pt-2 border-t-2 w-full">
           <div className="w-3/4 h-[600px] overflow-y-auto mx-auto">
-            {/* {Array.from({ length: gridRowCount }).map((_, rowIndex) => (
-                <div key={rowIndex} className="grid grid-cols-1 ml-1">
-                  {posts
-                    .slice(rowIndex * 3, rowIndex * 3 + 3) // 현재 행에 해당하는 게시글 추출
-                    .map((post) => (
-                      <div
-                        className="w-40 h-40 object-cover mt-1"
-                        key={post.id}
-                        onClick={() => router.push(`/posts/${post.id}`)}
-                      >
-                        <img src={post.thumbnail} alt="post.img" className="" />
-                      </div>
-                    ))}
-                </div>
-              ))} */}
-            <div className="flex flex-row flex-wrap justify-center gap-x-4 gap-y-4">
-              {/* {posts.map((post) => (
+            <div className="flex flex-row flex-wrap justify-start gap-x-4 gap-y-4">
+              {posts.map((post) => (
                 <div key={post.id} className="w-40 h-40" onClick={() => router.push(`/posts/${post.id}`)}>
                   <div style={{ position: "relative", width: "100%", height: "100%" }}>
                     <img
@@ -223,32 +243,9 @@ export default function UserInfo(): JSX.Element {
                     />
                   </div>
                 </div>
-              ))} */}
-              {userPosts.posts.map((post) => (
-                <div
-                  key={post.id}
-                  className="w-40 h-40 object-cover mt-4"
-                  onClick={() => router.push(`/posts/${post.id}`)}
-                >
-                  <div style={{ position: "relative", width: "100%", height: "100%" }}>
-                    <img
-                      src={post.thumbnail}
-                      alt="post.img"
-                      style={{ objectFit: "cover", width: "100%", height: "100%" }}
-                    />
-                  </div>
-                </div>
               ))}
-            </div>
-            {hasNextPage && (
-              <button
-                onClick={() => fetchNextPage()}
-                disabled={isFetchingNextPage}
-                className="bg-blue-500 text-white px-4 py-2 mt-4 rounded disabled:bg-gray-300"
-              >
-                {isFetchingNextPage ? "로딩 중..." : "더 불러오기"}
-              </button>
-            )}
+            </div>{" "}
+            <div id="scroll-trigger" style={{ position: "fixed", bottom: 0, height: 1 }} />
           </div>
         </div>
       </div>
