34f06095957db61db6a51aac17061978568d33d0
Eunseok Choi <xnelb013@naver.com>
2024-06-28 23:18:50 +0900

    commit6_28

--- pages/postEditor/GoogleMap.tsx
+++ pages/postEditor/GoogleMap.tsx
@@ -98,7 +98,7 @@ const GoogleMapsComponent = () => {
                     className="py-2 border-b border-gray-300 w-full"
                   />
                   <div className="h-[300px]">
-                    {loading ? <div>Loading...</div> : null}
+                    {loading ? <div>.</div> : null}
                     {suggestions.map((suggestion) => {
                       const style = suggestion.active
                         ? { backgroundColor: "#42a5f5", cursor: "pointer" }
